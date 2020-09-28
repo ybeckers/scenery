99fd22b13df31700cf1a42baa275a40aef67c126
ybeckers <71328741+ybeckers@users.noreply.github.com>
2020-09-28 22:47:34 +0200

    Uploaded Scenery

---
+++ js/sketch.js
@@ -0,0 +1,204 @@
+function setup() {
+  createCanvas(800,800);
+  background(42,76,100);
+  // put setup code here
+
+// gebouw 3-2
+noStroke();
+fill(100,166,212);
+rect(385, 50, 280, 800);
+
+ // gebouw 3-1
+ noStroke();
+ fill(66, 118, 173);
+ rect(385, 50, 140, 800);
+
+ // witte strepen gebouw 3
+
+ noStroke();
+fill(233,242,246);
+rect(410, 90, 225, 30);
+
+noStroke();
+fill(233,242,246);
+rect(410, 150, 225, 30);
+
+noStroke();
+fill(233,242,246);
+rect(410, 205, 225, 30);
+
+noStroke();
+fill(233,242,246);
+rect(410, 260, 225, 30);
+
+noStroke();
+fill(233,242,246);
+rect(410, 315, 225, 30);
+
+noStroke();
+fill(233,242,246);
+rect(410, 370, 225, 30);
+
+noStroke();
+fill(233,242,246);
+rect(410, 425, 225, 30);
+
+noStroke();
+fill(233,242,246);
+rect(410, 480, 225, 30);
+
+noStroke();
+fill(233,242,246);
+rect(410, 535, 225, 30);
+
+noStroke();
+fill(233,242,246);
+rect(410, 590, 225, 30);
+
+
+
+ // gebouw 2
+noStroke();
+fill(100,166,212);
+rect(325, 625, 175, 300);
+
+// witte strepen gebouw 2
+
+noStroke();
+fill(233,242,246);
+rect(250, 675, 225, 25);
+
+noStroke();
+fill(233,242,246);
+rect(250, 725, 225, 25);
+
+noStroke();
+fill(233,242,246);
+rect(250, 775, 225, 25);
+
+noStroke();
+fill(233,242,246);
+rect(275, 625, 225, 20);
+
+  // gebouw 1
+noStroke();
+fill(66, 118, 173);
+rect(50, 250, 275, 600);
+
+
+// gebouw 4
+stroke(233,242,246)
+strokeWeight(3)
+fill(100,166,212);
+ellipse(635, 500, 125);
+
+noStroke();
+fill(66, 118, 173);
+rect(525, 500, 220, 500);
+
+// witte vlakken gebouw 4
+
+noStroke();
+fill(233,242,246);
+rect(560, 525, 30, 260);
+
+noStroke();
+fill(233,242,246);
+rect(600, 525, 30, 260);
+
+noStroke();
+fill(233,242,246);
+rect(640, 525, 30, 260);
+
+noStroke();
+fill(233,242,246);
+rect(680, 525, 30, 260);
+
+noStroke();
+fill(233,242,246);
+rect(525, 490, 220, 10);
+
+
+
+// witte vlakken gebouw 1
+noStroke();
+fill(233,242,246);
+rect(225, 200, 90, 50);
+
+noStroke();
+fill(233,242,246);
+rect(75, 275, 225, 25);
+
+noStroke();
+fill(233,242,246);
+rect(25, 325, 325, 40);
+
+noStroke();
+fill(233,242,246);
+rect(25, 385, 325, 40);
+
+noStroke();
+fill(233,242,246);
+rect(75, 765, 225, 40);
+
+noStroke();
+fill(233,242,246);
+rect(95, 465, 35, 45);
+
+noStroke();
+fill(233,242,246);
+rect(145, 465, 35, 45);
+
+noStroke();
+fill(233,242,246);
+rect(195, 465, 35, 45);
+
+noStroke();
+fill(233,242,246);
+rect(245, 465, 35, 45);
+
+noStroke();
+fill(233,242,246);
+rect(95, 530, 35, 45);
+
+noStroke();
+fill(233,242,246);
+rect(145, 530, 35, 45);
+
+noStroke();
+fill(233,242,246);
+rect(195, 530, 35, 45);
+
+noStroke();
+fill(233,242,246);
+rect(245, 530, 35, 45);
+
+noStroke();
+fill(233,242,246);
+rect(95, 595, 35, 45);
+
+noStroke();
+fill(233,242,246);
+rect(145, 595, 35, 45);
+
+noStroke();
+fill(233,242,246);
+rect(195, 595, 35, 45);
+
+noStroke();
+fill(233,242,246);
+rect(245, 595, 35, 45);
+
+
+
+
+
+
+
+
+
+}
+
+function draw() {
+  // put drawing code here
+}
